8238479e35bb04a5d53c273fd8674098a7abd7c6
Hlamidomonada <60792528+Hlamidomonada@users.noreply.github.com>
2020-05-01 20:29:42 +0300

    Update const.hpp

--- const.hpp
+++ const.hpp
@@ -1,1 +1,8 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
 
+auto W = sf::VideoMode::getDesktopMode().width;
+auto H =sf::VideoMode::getDesktopMode().height;
+
+float DEGTORAD = 0.017453f;
+#endif
