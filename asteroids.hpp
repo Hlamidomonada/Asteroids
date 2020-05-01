a2f9d559091eac34c4a4e8435f4cd09686e46f20
Hlamidomonada <60792528+Hlamidomonada@users.noreply.github.com>
2020-05-01 21:25:39 +0300

    Update asteroids.hpp

--- asteroids.hpp
+++ asteroids.hpp
@@ -1,1 +1,15 @@
+#ifndef _ASTEROIDS_HPP
+#define _ASTEROIDS_HPP
 
+#include "entity.cpp"
+#include "const.hpp"
+
+class asteroid : public Entity
+{
+public:
+	asteroid();
+
+	void update();
+	
+};
+#endif
