775e8bb47a73456643a6a730f2013fc14e4780cd
Hlamidomonada <60792528+Hlamidomonada@users.noreply.github.com>
2020-05-04 15:42:31 +0300

    Update entity.hpp

--- entity.hpp
+++ entity.hpp
@@ -15,11 +15,11 @@ public:
 
 	Entity();
 
-	void settings(std::unique_ptr<Animation> a, int X, int Y, float Angle = 0, int radius = 1);
+	void settings(Animation& a, int X, int Y, float Angle = 0, int radius = 1);
 
   virtual void update();
 
-	void draw(std::unique_ptr<RenderWindow> app);
+	void draw(RenderWindow& app);
 
 	virtual ~Entity();
 };
