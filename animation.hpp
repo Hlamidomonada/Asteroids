f4eab299dc757767c01317808e868b2ffdafd52b
Jens <37579885+i-s-m-mipt@users.noreply.github.com>
2020-05-04 16:47:43 +0300

    Добавлены комментарии

--- animation.hpp
+++ animation.hpp
@@ -1,22 +1,22 @@
 #ifndef _ANIMATION_HPP
-#define _ANIMATION_HPP
+#define _ANIMATION_HPP // можно pragma once
 
 #include <SFML/Graphics.hpp>
 
 class Animation
 {
 public:
-	float Frame, speed;
-	Sprite sprite;
-	std::vector<IntRect> frames;
+	float Frame, speed; // почему данные публичные? Frame с ББ?
+	Sprite sprite; // именование с m_ лучше делать
+	std::vector<IntRect> frames; // стоит добавить include vector
 
-	Animation();
+	Animation(); // тут мы обычно пишем = default, но он вообще нужен?
   
 	Animation(Texture& t, int x, int y, int w, int h, int count, float Speed);
 
   void update();
 
-	bool isEnd();
+	bool isEnd(); // надо добавить const, т.к. функция не изменяет объект
 	
 
 };
