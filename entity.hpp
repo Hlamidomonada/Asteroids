dbb38db46256955df1529e9fe112d7efebbf0f4a
Jens <37579885+i-s-m-mipt@users.noreply.github.com>
2020-05-04 16:59:05 +0300

    Добавлены комментарии

--- entity.hpp
+++ entity.hpp
@@ -8,19 +8,19 @@
 class Entity
 {
 public:
-	float x, y, dx, dy, R, angle;
-	bool life;
+	float x, y, dx, dy, R, angle; // публичные данные?
+	bool life; // именование с мб и с m_ в начале
 	std::string name;
 	Animation anim;
 
-	Entity();
+	Entity(); // = default?
 
-	void settings(Animation& a, int X, int Y, float Angle = 0, int radius = 1);
+	void settings(Animation& a, int X, int Y, float Angle = 0, int radius = 1); // переменные с Бб
 
-  virtual void update();
+  virtual void update(); // табуляция съехала
 
 	void draw(RenderWindow& app);
 
-	virtual ~Entity();
+	virtual ~Entity(); // = default?
 };
 #endif
