814b7190141048457c00403838e8c927a0a5b162
Jens <37579885+i-s-m-mipt@users.noreply.github.com>
2020-05-04 16:54:58 +0300

    Добавлены комментарии

--- const.hpp
+++ const.hpp
@@ -1,8 +1,8 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
-
+// константы так и остались глобальными
 auto W = sf::VideoMode::getDesktopMode().width;
-auto H =sf::VideoMode::getDesktopMode().height;
+auto H =sf::VideoMode::getDesktopMode().height; // как эти строки работают, если даже нет нужного хедера?
 
-float DEGTORAD = 0.017453f;
+float DEGTORAD = 0.017453f; // имя константы выглядит как имя макроса
 #endif
